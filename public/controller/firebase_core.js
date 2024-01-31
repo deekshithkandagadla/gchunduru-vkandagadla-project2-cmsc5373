a9d6685714e9561f25050aae6b8e40f7ff66b709
deekshithkandagadla <deekshith.kandagadla@gmail.com>
2024-01-31 15:31:45 -0600

    init

---
+++ public/controller/firebase_core.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBi9U0JL47CqYDYf6wllasaSuBcqhO8SZM",
+    authDomain: "vkandagadla-cmsc5373-clwebapp.firebaseapp.com",
+    projectId: "vkandagadla-cmsc5373-clwebapp",
+    storageBucket: "vkandagadla-cmsc5373-clwebapp.appspot.com",
+    messagingSenderId: "961778724253",
+    appId: "1:961778724253:web:c5b234e66ac5750e411374"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
