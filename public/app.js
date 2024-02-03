81dc5dad27676f999f08e74f543cd03c64b9dcc8
deekshithkandagadla <deekshith.kandagadla@gmail.com>
2024-02-02 19:05:59 -0600

    commit

--- public/app.js
+++ public/app.js
@@ -1,1 +1,26 @@
-console.log('from app.js')
+import { attachOnAuthStateChangeObserver } from "./controller/firebase_auth.js";
+import { onClickHomeMenu, onClickMenu2Menu, onClickSignOutMenu } from "./controller/menueventhandlers.js";
+import { routing } from "./controller/route_controller.js";
+
+
+//menu button handler
+
+document.getElementById('menu-home').onclick = onClickHomeMenu
+document.getElementById('menu-menu2').onclick = onClickMenu2Menu;
+document.getElementById('menu-signout').onclick = onClickSignOutMenu;
+
+attachOnAuthStateChangeObserver();
+
+window.onload = function(e) {
+    const pathname = window.location.pathname;
+    const hash = window.location.hash;
+    //console.log('pathname:', pathname, hash);
+    routing(pathname, hash)
+}
+
+window.onpopstate = function(e) {
+    e.preventDefault();
+    const pathname = window.location.pathname;
+    const hash = window.location.hash;
+    routing(pathname, hash);
+}
